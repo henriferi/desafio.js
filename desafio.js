c6ed27d99026a3c28c4aa901c1d048897ed8bdc1
Henrique <henriquefernandes.gouveia@gmail.com>
2021-09-25 09:56:25 -0300

    first commit

---
+++ desafio.js
@@ -0,0 +1,15 @@
+const mensagemSecreta = ["Aprender", "não", "é", "sobre", "o", "que", "você", "consegue", "facilmente", "na", "primeira vez", "mas", "sobre", "o", "que", "você", "pode", "descobrir", "- 2015", "Chris", "Pine", "Aprenda", "JavaScript"];
+mensagemSecreta.pop();
+mensagemSecreta.push("a", "programar");
+mensagemSecreta.shift();
+mensagemSecreta.unshift("Programação");
+mensagemSecreta.splice(7, 4, "sabe");
+
+console.log(mensagemSecreta.join(" "));
+
+
+
+
+
+
+
